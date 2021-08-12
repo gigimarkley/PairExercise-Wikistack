9a6d95e51f9bbfdaf10bb26df3d012c6ec83eeef
Miranda Stopa <miranda.stopa@gmail.com>
2021-08-12 13:03:33 -0400

    connected to postgres

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
-const path = require('path');
+const path = require("path");
+const layout = require("./views/layout");
+const { db } = require("./models");
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/',(req,res,next) => {
-  res.send(console.log('Hello World!'))
-})
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+
+app.get("/", (req, res, next) => {
+  res.send(layout(""));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`app listening at http://localhost:${PORT}`);
+});
